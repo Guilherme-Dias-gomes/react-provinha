9939b91acd543f43fefa35d70622b20f52ee63fe
Guilherme Dias Gomes <guilhermediasgomes2@gmail.com>
2022-05-25 11:36:28 -0300

    alterações

--- src/pages/corPrimaria/index.js
+++ src/pages/corPrimaria/index.js
@@ -1,13 +1,23 @@
+import axios from 'axios'
+import { useState } from "react"
 
 
 
 export default function index() {
+    const [cor,  setCor] = useState('');
+
     return(
         <main>
             <h2>Cor Primária</h2>
 
             <div>
                Cor: <input type='text' value={cor} onChange={e => setCor(e.target.value)} />
+            </div>
+            <div>
+                <button> Verificar </button>
+            </div>
+            <div>
+                É uma cor primaria ?
             </div>
         </main>
     )
