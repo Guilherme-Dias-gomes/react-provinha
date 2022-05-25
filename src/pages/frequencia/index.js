fa88e95ceaeb3e6a8965174142a6889ac85a11e2
Guilherme Dias Gomes <guilhermediasgomes2@gmail.com>
2022-05-25 14:00:42 -0300

    mensagem

--- src/pages/frequencia/index.js
+++ src/pages/frequencia/index.js
@@ -1,7 +1,32 @@
+import axios from 'axios'
+import { useState } from "react"
+
 export default function Index() {
+    const [texto, setTexto] = useState('');
+    const [Caractere, setCaractere] = useState('');
+    const [resposta, setResposta] = useState('');
+
+        async function verificarFreq() {
+            const resp = await axios.get(`http://localhost:5000/dia2/frequencia/${texto}/${Caractere}`)
+            setResposta(resp.data.x);
+        }
+
     return(
         <main>
             <h2>Frequência</h2>
+
+            <div>
+                texto: <input type='text' value = {texto} onChange={e => setTexto(e.target.value)} />
+            </div>
+            <div>
+                Caractere: <input type='text' value={Caractere} onChange={e => setCaractere(e.target.value)} />
+            </div>
+            <div>
+                <button onClick={verificarFreq}> Verificar Frequência</button>
+            </div>
+            <div>
+                Frequência: {resposta}
+            </div>
         </main>
     )
 }
