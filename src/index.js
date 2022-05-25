328a4e206c9af17ed3d0d42d025a8f0759f9da81
David Douglas Gonçalves Ribeiro <davidddouglas1042@gmail.com>
2022-05-25 10:18:59 -0300

    Att

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import SiteRoutes from './siteRoutes';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SiteRoutes />
+    <SiteRoutes/>
   </React.StrictMode>
 );
 
